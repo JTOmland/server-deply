93d9c0eb6db8170c9594d9f0c75583f3d84f50cc
Jeff Omland <jeff.omland@gmail.com>
2017-06-15 19:52:04 -0500

    added html

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
-app.get('/', function(req,res){
-    res.send("Big Hey From Deployed Code after modified and pushed")
-});
+app.use(express.static('public'))
 
 app.listen(9000, function(){
     console.log('Server running on port 9000')
